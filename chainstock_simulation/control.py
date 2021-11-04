7242584df83c1bf4d6ebec20e4751bdf3e2d3f1c
luuk gijben <l.gijben@chain-stock.com>
2021-11-04 16:34:48 +0100

    Created order creation.
    The order creation includes an example of order creation via the R,s,Q control policy.

--- chainstock_simulation/control.py
+++ chainstock_simulation/control.py
@@ -1,4 +1,12 @@
-def r_s_q(d, t):
+from math import ceil
 
-    if not t % d["review_time"] == 0:
-        pass
+
+def r_s_q(d, inventory, t):
+    order_quantity = 0
+    if (t % d["review_time"] == 0) & (inventory < d["reorder_level"]):
+        order_quantity = (
+            ceil((d["reorder_level"] - inventory) / d["order_quantity"])
+            * d["order_quantity"]
+        )
+
+    return order_quantity
