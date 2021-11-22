3f5c3178c5efeb082d540ef3966c02bb45a82738
Allex Veldman <a.veldman@chain-stock.com>
2021-11-22 13:18:25 +0100

    Add unittests for control and release strategies

---
+++ tests/supplychain_simulation/strategy/release/test_fractional.py
@@ -0,0 +1,59 @@
+from supplychain_simulation import Node
+from supplychain_simulation.node import Orders, Stock
+from supplychain_simulation.strategy import Fractional
+
+
+def test_fractional_feasible():
+    orders = {
+        "A": 20,
+        "B": 40,
+    }
+
+    stock = 70
+
+    node = Node("A", orders=Orders(**orders), stock=Stock({"A": stock}))
+
+    order_release = Fractional().get_releases(node=node)
+
+    assert order_release == {
+        "A": 20,
+        "B": 40,
+    }
+
+
+def test_fractional_infeasible():
+    """
+    Test whereby the sum of orders requested is larger than the available stock.
+    Furthermore, the rounding causes the initial sum of released orders to be larger than the available stock.
+    """
+    orders = {
+        "A": 20,
+        "B": 40,
+    }
+    stock = 7
+
+    node = Node("A", orders=Orders(**orders), stock=Stock({"A": stock}))
+
+    order_release = Fractional().get_releases(node=node)
+
+    assert order_release == {
+        "A": 3,
+        "B": 4,
+    }
+
+
+def test_fractional_no_order():
+    """
+    Test whereby the sum of orders requested is larger than the available stock.
+    Furthermore, the rounding causes the initial sum of released orders to be larger than the available stock.
+    """
+    orders = {
+        "A": 0,
+        "B": 0,
+    }
+    stock = 7
+    node = Node("A", orders=Orders(**orders), stock=Stock({"A": stock}))
+
+    order_release = Fractional().get_releases(node=node)
+
+    assert order_release == {}
